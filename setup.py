611402d0d38b7042079e22b0f3f93f0d1254357d
dekaottoman <tberkayayaz@gmail.com>
2025-02-05 00:24:47 +0300

    Updated pyproject.toml and setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
     name="clustanom",
-    version="0.0.1",
+    version="0.1.0",
     author="Teoman Berkay Ayaz",
-    author_email="your_email@example.com",
-    description="ClustAnom is a Scikit-Learn compatible, clustering based anomaly detection library.",
+    author_email="tberkayayaz@gmail.com",
+    description="ClustAnom is a Scikit-Learn compatible, clustering-based anomaly detection library.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/dekaottoman/clustanom",
-    packages=find_packages(),
+    packages=["clustanom"],
     install_requires=[
         "numpy",
+        "scipy",
         "scikit-learn",
     ],
     classifiers=[
@@ -18,6 +20,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.8",
+    license="MIT",
 )
-
